69f8a7c2c48952e8ca1e5ed700a470b1418eca90
Renish B <renishb10@gmail.com>
2023-08-18 11:51:48 +0530

    naive string search

--- code/21_naive_string_search.js
+++ code/21_naive_string_search.js
@@ -2,4 +2,15 @@
 Q: Write a program that returns the frequency of short string occurs anywhere in the given long string.
 */
 
-function naiveSearch(long, short) {}
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (long[i + j] !== short[j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("laugh out loud is lol and lol is fun", "lol"));
